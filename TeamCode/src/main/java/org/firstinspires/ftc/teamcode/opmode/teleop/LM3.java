abb92f2a1642a60135163e43ca09ddda50e2f546
Ddundee <dhanush.actual1@gmail.com>
2024-01-19 20:34:40 -0600

    zaddy shrestttttttt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/LM3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/LM3.java
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.photoncore.Photon;
 import org.firstinspires.ftc.teamcode.subsystem.Claw;
 import org.firstinspires.ftc.teamcode.subsystem.Hang;
 import org.firstinspires.ftc.teamcode.subsystem.Robot;
@@ -11,11 +15,12 @@ import org.firstinspires.ftc.teamcode.util.LoopRateTracker;
 import org.firstinspires.ftc.teamcode.util.gamepad.JustPressed;
 
 @TeleOp(name = "LM3 Game", group = "game")
+@Photon
 public class LM3 extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        LoopRateTracker loopRateTracker;
-        Robot robot = new Robot(hardwareMap, telemetry, Claw.ClawState.OPEN, Hang.HangState.DOWN, true, AllianceColor.BLUE);
+        telemetry = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry(), telemetry);
+        Robot robot = new Robot(hardwareMap, telemetry, Claw.ClawState.OPEN, Hang.HangState.DOWN, AllianceColor.BLUE);
         JustPressed gp1 = new JustPressed(gamepad1);
         JustPressed gp2 = new JustPressed(gamepad2);
 
@@ -27,17 +32,16 @@ public class LM3 extends LinearOpMode {
 
         waitForStart();
 
-        loopRateTracker = new LoopRateTracker();
 
         while (opModeIsActive()) {
+
             robot.clearCache();
             robot.update();
-            loopRateTracker.update();
 
             robot.drive.setPowers(gp1.left_stick_x(), gp1.left_stick_y(), gp1.right_stick_x(), x -> (Math.pow(x, power) * .75 * Math.signum(x)));
 
             robot.getTelemetry();
-            robot.telemetry.addData(new HtmlFormatter().textColor("red").format("LoopRate"), loopRateTracker.getLoopTime() + "ms");
+//            robot.telemetry.addData(new HtmlFormatter().textColor("red").format("LoopRate"), loopRateTracker.getLoopTime() + "ms");
             robot.telemetry.update();
 
             gp1.update();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Arm.java
@@ -5,6 +5,7 @@ import com.acmerobotics.roadrunner.control.PIDFController;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.justbetter.actuator.CachingDcMotorEX;
@@ -14,89 +15,25 @@ import org.jetbrains.annotations.NotNull;
 
 public class Arm {
 
-    private final CachingDcMotorEX motor1, motor2;
+    public final DcMotorEx motor1;
+    public final DcMotorEx motor2;
 
-    private final PIDController differenceController, averageController;
-
-    private GravityFeedforward gravityFeedforward;
-
-    private int extensionTargetPosition = 0;
-    private int pivotTargetPosition = 0;
-    private int motor1Position, motor2Position;
-
-    private double difference, average, differencePower, averagePower, gravityPower;
 
     public Arm(HardwareMap hardwareMap) {
-        motor1 = new CachingDcMotorEX(hardwareMap.get(DcMotorEx.class, Constants.Arm.MOTOR1_MAP_NAME));
-        motor2 = new CachingDcMotorEX(hardwareMap.get(DcMotorEx.class, Constants.Arm.MOTOR2_MAP_NAME));
+        motor1 = hardwareMap.get(DcMotorEx.class, Constants.Arm.MOTOR1_MAP_NAME);
+        motor2 = hardwareMap.get(DcMotorEx.class, Constants.Arm.MOTOR2_MAP_NAME);
 
+//        motor2.setDirection(DcMotorSimple.Direction.REVERSE);
         for(DcMotorEx motor : new DcMotorEx[]{motor1, motor2}) {
             motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            motor.setDirection(DcMotorSimple.Direction.REVERSE);
         }
-
-        differenceController = new PIDController(Constants.Arm.AVERAGE_PID_COEFFICIENTS.kP, Constants.Arm.AVERAGE_PID_COEFFICIENTS.kI, Constants.Arm.AVERAGE_PID_COEFFICIENTS.kD);
-        averageController = new PIDController(Constants.Arm.DIFFERENCE_PID_COEFFICIENTS.kP, Constants.Arm.DIFFERENCE_PID_COEFFICIENTS.kI, Constants.Arm.DIFFERENCE_PID_COEFFICIENTS.kD);
-        gravityFeedforward = new GravityFeedforward(Constants.Arm.GRAVITY_FEEDFORWARD_GAIN);
-    }
-
-    public void setGravityFeedforward(@NotNull double gain) {
-        gravityFeedforward.setGain(gain);
-    }
-
-    public void setDifferenceControllerPID(@NotNull PIDCoefficients pid) {
-        differenceController.setPID(pid.kP, pid.kI, pid.kD);
-    }
-
-    public void setAverageControllerPID(@NotNull PIDCoefficients pid) {
-        averageController.setPID(pid.kP, pid.kI, pid.kD);
-    }
-
-    public void setExtensionTargetPosition(@NotNull int targetPosition) {
-        extensionTargetPosition = targetPosition;
-    }
-
-    public void setPivotTargetPosition(@NotNull int targetPosition) {
-        pivotTargetPosition = targetPosition;
-    }
-
-    public int getExtensionTargetPosition() {
-        return extensionTargetPosition;
-    }
-
-    public int getPivotTargetPosition() {
-        return pivotTargetPosition;
-    }
-
-    public int getPivotCurrentPosition() {
-        return (motor1Position + motor2Position)/2;
-    }
-
-    public int getExtensionCurrentPosition() {
-        return  motor1Position - motor2Position;
-    }
-
-    public double getGravityFFPower() {
-        return gravityPower;
     }
 
     public void update() {
-        motor1Position = motor1.getCurrentPosition();
-        motor2Position = motor2.getCurrentPosition();
 
-        difference = motor1Position - motor2Position; // todo figure out if this is reversed
-        average = (double) (motor1Position + motor2Position) / 2.0;
-
-        differencePower = differenceController.calculate(difference, extensionTargetPosition);
-        averagePower = averageController.calculate(average, extensionTargetPosition);
-        gravityPower = gravityFeedforward.calculate(ticksToMillimeters(difference), Math.toRadians(ticksToDegrees(average)));
-
-        double power1 = differencePower + averagePower + gravityPower;
-        double power2 = -differencePower + averagePower - gravityPower;
-
-        motor1.setPower(power1);
-        motor2.setPower(power2);
     }
 
     public static double backdropYtoX(double y) {
