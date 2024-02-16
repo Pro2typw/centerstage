bc6add84a6cad2aa89521ec554c01b3e4b49dff5
jayant <jbhaskaruni@gmail.com>
2024-02-15 19:35:30 -0600

    far side paths

--- MeepMeep/src/main/java/com/example/meepmeep/lmt/PathFullTest.java
+++ MeepMeep/src/main/java/com/example/meepmeep/lmt/PathFullTest.java
@@ -12,42 +12,29 @@ public class PathFullTest {
         final double LENGTH = 18;
 
 
-        final Pose2d StartingPose = new Pose2d(12, -63, Math.toRadians(270));
+        final Pose2d startingPose = new Pose2d(-39, 63, Math.toRadians(90));
 
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 .setDimensions(14.2, LENGTH )
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(0), 12.5)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(StartingPose)
+                        drive.trajectorySequenceBuilder(startingPose)
                                 .setReversed(true)
-                                .addDisplacementMarker(() -> {
-                                    // point toward backdrop ready to drop
-//                                    robot.wrist.setPosition(Constants.Wrist.INTAKE_POS);
-                                })
-                                .lineTo(new Vector2d(15, -51))
-                                .lineToLinearHeading(new Pose2d(15, -33, Math.toRadians(270)))
+                                .lineTo(new Vector2d(-36, 51))
+                                .lineToLinearHeading(new Pose2d(-36, 35, Math.toRadians(270)))
                                 .addDisplacementMarker(() -> {
                                     // claw drop purple
-//                                    robot.claw.setClawState(Claw.ClawSide.LEFT, Claw.ClawState.OPEN);
-                                })
-                                .waitSeconds(.5)
-                                .lineTo(new Vector2d(12.1, -36))
-                                .addDisplacementMarker(() -> {
-//                                    robot.arm.setPivotTargetPos(150);
                                 })
                                 .waitSeconds(1)
-                                .lineTo(new Vector2d(36, -36))
+                                .lineToLinearHeading(new Pose2d(-36, 60, Math.toRadians(180)))
+
+                                .setReversed(true)
+                                .lineTo(new Vector2d(12, 60))
+                                .splineTo(new Vector2d(48, 36), 0)
                                 .addDisplacementMarker(() -> {
-                                    // pivot to depo
-//                                    robot.arm.setPivotTargetPos(580);
-//                                    robot.wrist.setPosition(Constants.Wrist.DEPO_POS);
-//                                    robot.claw.setClawState(Claw.ClawSide.LEFT, Claw.ClawState.CLOSE);
-                                })
-                                .lineToLinearHeading(new Pose2d(48, -36, Math.toRadians(180)))
-                                .addDisplacementMarker(() -> {
-                                    // claw drop yellow
-//                                    robot.claw.setClawState(Claw.ClawSide.RIGHT, Claw.ClawState.OPEN);
+                                    // pivot and drop yellow pixel on left backboard
+                                    // park (just stay in front of backboard)
                                 })
                                 .build()
                 );
