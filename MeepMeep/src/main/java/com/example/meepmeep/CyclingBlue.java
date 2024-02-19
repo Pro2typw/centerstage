c996540bc3414680d04d90a2aee90aa35bb5a1c8
jayant <jbhaskaruni@gmail.com>
2024-02-18 22:04:02 -0600

    far side paths rr1.0 version

--- MeepMeep/src/main/java/com/example/meepmeep/CyclingBlue.java
+++ MeepMeep/src/main/java/com/example/meepmeep/CyclingBlue.java
@@ -1,19 +1,13 @@
-package com.example.meepmeep.prop.stack;
+package com.example.meepmeep;
 
-import com.acmerobotics.roadrunner.Arclength;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Pose2dDual;
-import com.acmerobotics.roadrunner.PosePath;
 import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.VelConstraint;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.DriveTrainType;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-import org.jetbrains.annotations.NotNull;
-
-public class BlueCenter {
+public class CyclingBlue {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
@@ -23,24 +17,31 @@ public class BlueCenter {
                 .setDimensions(15.8, 17.5)
                 .build();
 
-        Pose2d startPose = new Pose2d(-39, 61.5, Math.toRadians(90));
+        Pose2d startPose = new Pose2d(47, 34, Math.toRadians(180));
 
         myBot.runAction(myBot.getDrive().actionBuilder(startPose)
-                // wrist init -> intake
-                .lineToYSplineHeading(36,  Math.toRadians(270))
-                // drop purple
-                .lineToYSplineHeading(35, Math.toRadians(180))
-                .setTangent(270)
-                .lineToXSplineHeading(-54, Math.toRadians(180))
-                //pickup white pixel from stack
-                .setTangent(0)
-                .splineToConstantHeading(new Vector2d(-39, 58), Math.toRadians(90))   //-39, 58
+                .setTangent(Math.toRadians(180))
+                .splineTo(new Vector2d(22,58), Math.toRadians(180))
+
+
+                .lineToX(-39)
+
+
+                .strafeTo(new Vector2d(-39, 35))
+                // pickup white pixel from stack
+
+
+                .strafeTo(new Vector2d(-39, 58))
 
                 .setTangent(Math.toRadians(180))
-                        .lineToX(22)
+                .lineToX(22)
 
                 .splineTo(new Vector2d(47,34), Math.toRadians(0))
 
+
+                // if parking...
+                .setTangent(Math.toRadians(90))
+                .lineToY(60)
 
                 .build());
 
